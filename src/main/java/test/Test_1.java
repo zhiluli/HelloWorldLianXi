3072574469a662d954aacec9bd20cb751dce1c26
zhiluli <q18538646642@163.com>
2020-05-08 21:32:25 +0800

    第二次  失败的冒泡排序  网上的冒泡排序

--- src/main/java/test/Test_1.java
+++ src/main/java/test/Test_1.java
@@ -2,22 +2,54 @@ package test;
 
 import org.junit.Test;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Test_1 {
     //冒泡排序
     //这里不对明天有时间再调 2020年5月6日
     @Test
-    public void test1(){
-        int score[] = {6, 9, 5, 8};
-        for(int i =0;i < score.length - 1;i++) {
-            // j开始等于0，
-            for(int j = 0;j <  score.length - 1-i;j++){
-                if(score[j] < score[j+1]) {
-                    int temp = score[j];
-                    score[j] = score[j+1];
-                    score[j+1] = temp;
-                    System.out.print(temp);
-                }
-            }
-        }
+    public static void main(String[] args) {
+        int arr[]={5,1,2,-2,8,4,7};
+        sort(arr,0,arr.length-1);
+        System.out.println(Arrays.toString(arr));
     }
+    //这个方法的三个参数：arr要排序的那个数组，  你要对arr[low]-arr[high]段进行排序
+    public static void sort(int[] arr,int low,int high){
+        if (low>=high){
+            return;
+        }
+        // i  j
+        int i=low;
+        int j=high;
+        //key
+        int key=arr[i];
+       while (i<j){
+           while (arr[j]>=key&&i<j){
+               j--;
+           }
+         if (i<j){
+             int t;
+             t=arr[j];
+             arr[j]=arr[i];
+             arr[i]=t;
+         }
+
+           while (arr[i]<=key&&i<j){
+               i++;
+           }
+          if (i<j){
+               int t;
+              t=arr[i];
+              arr[i]=arr[j];
+              arr[j]=t;
+          }
+       }
+       //对基准左侧的集合重复操作
+        sort(arr,low,i-1);
+       //对基准右侧的集合重复操作
+        sort(arr,i+1,high);
+    }
+
 }
